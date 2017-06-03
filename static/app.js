aa3e6253241e89b74fc5d5152931b624a77fb5bc
Kyle <skz.zxc@gmail.com>
2017-06-03 12:29:31 -0400

    hackish getaround to have stuff on screen

--- static/app.js
+++ static/app.js
@@ -13,11 +13,6 @@ var GameLayer = cc.Layer.extend({
 		var background = new cc.LayerColor( cc.color(0, 0, 0) );
 		this.addChild( background, res.Z_ORDER_BG, res.TAG_BG );
 
-		this.conn = new WebSocket( "ws://localhost:8080/ws" );
-
-		this.conn.onmessage = this.onSocketMessage;
-		this.conn.onerror = this.onSocketError;
-
 		// hardcoded numbers.. how to retrieve from server?
 		for (var i = 0; i < 5; i++) {
 			this.colors.push( "black" );
@@ -36,6 +31,27 @@ var GameLayer = cc.Layer.extend({
 
 		this.drawNode = new cc.DrawNode();
 		this.addChild( this.drawNode );
+
+		this.conn = new WebSocket( "ws://localhost:8080/ws" );
+		//this.conn.onmessage = this.onSocketMessage;
+
+		// hackish workaround because in would be func onSocketMessage, this no longer = cc Layer
+		var app = this;
+		this.conn.onmessage = function (evt) {
+			var gameInfo = JSON.parse( evt.data );
+			for (var i = 0; i < gameInfo.Colors.length; i++) {
+				app.colors[i] = gameInfo.Colors[i];
+			}
+			for (var i = 0; i < gameInfo.Grid.length; i++) {
+				for (var j = 0; j < gameInfo.Grid[i].length; j++) {
+					app.grid[ i * gameInfo.Grid.length + j ] = gameInfo.Grid[i][j];
+				}
+			}
+
+			app.reDraw();
+		};
+
+		this.conn.onerror = this.onSocketError;
 
 		if ('keyboard' in cc.sys.capabilities) {
 			cc.eventManager.addListener( {
@@ -70,25 +86,27 @@ var GameLayer = cc.Layer.extend({
 		//this.scheduleUpdate();
 	},
 
-	onSocketMessage:function (evt) {
+	/*onSocketMessage:function (evt) {
+		// i see.. once we're in this func 'this' no longer refers to the cc Layer
+
 		var gameInfo = JSON.parse( evt.data );
-		for (var i = 0; i < gameInfo.colors.length; i++) {
-			colors[i] = gameInfo.colors[i];
+		for (var i = 0; i < gameInfo.Colors.length; i++) {
+			this.colors[i] = gameInfo.Colors[i];
 		}
-		for (var i = 0; i < gameInfo.grid.length; i++) {
-			for (var j = 0; j < gameInfo.grid[i].length; j++) {
-				grid[ i * gameInfo.grid.length + j ] = gameInfo.grid[i][j];
+		for (var i = 0; i < gameInfo.Grid.length; i++) {
+			for (var j = 0; j < gameInfo.Grid[i].length; j++) {
+				this.grid[ i * gameInfo.Grid.length + j ] = gameInfo.Grid[i][j];
 			}
 		}
 
 		this.reDraw();
-	},
+	},*/
 
 	reDraw:function() {
 		// more hardcoding.. this is bad
 		for (var i = 0; i < 80; i++) {
 			for (var j = 0; j < 45; j++) {
-				this.drawNode.drawRect( cc.p( i * 10, j * 10 ), cc.p( (i + 1) * 10, (j + 1) * 10 ), 1, 
+				this.drawNode.drawRect( cc.p( i * 10, j * 10 ), cc.p( (i + 1) * 10, (j + 1) * 10 ), 0, 
 										this.colorLookup[ this.colors[ this.grid[ i * 80 + j ] ] ] );
 			}
 		}
